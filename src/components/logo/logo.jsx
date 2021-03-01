fd2d77b5c050c8ff5cc74c68648597e63ceaf686
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-02-28 20:52:03 -0500

    Adding in the logo text for navbar

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -8,11 +8,18 @@ const logo = {
   alt: "Elder Help NC",
   src: `${CDN_URL}/logo.png`
 };
+const text = {
+  alt: "Elder Help NC",
+  src: `${CDN_URL}/elder-help-text.png`
+};
 
 const Logo = () => (
   <div className="logo">
     <NextLink href="/">
       <img className="logo__image" src={logo.src} alt={logo.alt} />
+    </NextLink>
+    <NextLink href="/">
+      <img className="logo__text" src={text.src} alt={text.alt} />
     </NextLink>
   </div>
 );
