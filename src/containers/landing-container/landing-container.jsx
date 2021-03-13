39009b949b3d22e0ca2ab53120d34869f7e509c8
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-03-12 22:23:45 -0500

    Updating with feedback from design regarding having the curved bottom as transition

--- src/containers/landing-container/landing-container.jsx
+++ src/containers/landing-container/landing-container.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import goals from "../../content/goals";
 import Image from "../../atoms/image";
 import GoalWidget from "../../components/goal-widget";
+import LandingSvg from "../../components/landing-svg";
 import { getCdnUrl } from "../../utils";
 
 import "./landing-container.scss";
@@ -15,12 +16,20 @@ const splashImage = {
 
 const LandingContainer = () => (
   <div className="landing-container">
-    <Image className="landing-container__splash-image" {...splashImage} />
-    <h2 className="landing-container__title">Elder Help NC's Mission</h2>
-    <p className="landing-container__mission">
-      To develop, organize and provide Health, Education, Local resources and
-      Personal needs (HELP) to the elderly and disabled.
-    </p>
+    <div className="landing-container__splash">
+      <Image className="landing-container__splash-image" {...splashImage} />
+      <h2 className="landing-container__splash-title">
+        Elder Help NC's Mission
+      </h2>
+      <p className="landing-container__splash-mission">
+        To develop, organize and provide Health, Education, Local resources and
+        Personal needs (HELP) to the elderly and disabled.
+      </p>
+      <div className="landing-container__splash-divide">
+        <LandingSvg classname="svg__fill" />
+        <LandingSvg classname="svg__line" />
+      </div>
+    </div>
     <div className="landing-container__goals">
       {goals.map(({ key, imgSrc, title }) => (
         <GoalWidget
