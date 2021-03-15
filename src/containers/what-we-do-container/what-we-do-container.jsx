c15559307622913d2b20067127fc7ca5ed1b01f2
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-03-14 21:04:10 -0400

    Moving pieces from what-we-do to who we are per design

--- src/containers/what-we-do-container/what-we-do-container.jsx
+++ src/containers/what-we-do-container/what-we-do-container.jsx
@@ -1,41 +1,19 @@
 import React from "react";
 import Hyperlink from "../../atoms/hyperlink";
-import Image from "../../atoms/image";
-import { getCdnUrl } from "../../utils";
 
 import "./what-we-do-container.scss";
-
-const CDN_URL = getCdnUrl("/what-we-do");
-
-const chamberImage = {
-  altText: "Members of Elder Help with Chamber of Commerce member",
-  imgSrc: `${CDN_URL}/what-we-do-container__chamber.jpg`
-};
 
 const WhatWeDoContainer = () => (
   <div className="what-we-do-container">
     <h2 className="what-we-do-container__main-title">What We Do</h2>
     <p className="what-we-do-container__main-content">
-      Collaboration is at the core of everything we do. We know that no single
-      organization can solve the complex set of issues contributing to local and
-      national issues.
+      List of projects or initiatives here
     </p>
     <Hyperlink
       href="/take-action"
       title="Get Involved"
       className="what-we-do-container__cta"
     />
-    <figure className="what-we-do-container__chamber-figure">
-      <Image className="what-we-do-container__figure-image" {...chamberImage} />
-      <figcaption className="what-we-do-container__figure-caption">
-        Pictured with Chamber President, Holly Staples (Center), are President,
-        Dana Rabon (Right) and Vice President, Audrey Clark (Left)
-      </figcaption>
-    </figure>
-    <p className="what-we-do-container__secondary-content">
-      We champion partnerships that bring together expertise, ideas, tools and
-      resources in support of the shared goal of creating a better tomorrow.
-    </p>
   </div>
 );
 
--- src/containers/who-we-are-container/who-we-are-container.jsx
+++ src/containers/who-we-are-container/who-we-are-container.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Hyperlink from "../../atoms/hyperlink";
 import Image from "../../atoms/image";
 import { getCdnUrl } from "../../utils";
 
@@ -8,7 +9,17 @@ const CDN_URL = getCdnUrl("/who-we-are");
 
 const splashImage = {
   altText: "Elderly man with an aide looking at computer",
-  imgSrc: `${CDN_URL}/who-we-are-container__man-with-aide.jpg`
+  imgSrc: `${CDN_URL}/who-we-are-container__splash.jpg`
+};
+
+const visionImage = {
+  altText: "Conference where a girl is taking notes on a paper pad",
+  imgSrc: `${CDN_URL}/who-we-are-container__vision.jpg`
+};
+
+const chamberImage = {
+  altText: "Members of Elder Help with Chamber of Commerce member",
+  imgSrc: `${CDN_URL}/who-we-are-container__chamber.jpg`
 };
 
 const WhoWeAreContainer = () => (
@@ -19,14 +30,39 @@ const WhoWeAreContainer = () => (
       To develop, organize and provide Health, Education, Local resources and
       Personal needs (HELP) to the elderly and disabled.
     </p>
+    <Image className="who-we-are-container__vision-image" {...visionImage} />
     <h3 className="who-we-are-container__vision-title">Our Vision</h3>
     <p className="who-we-are-container__vision-content">
       Elder Help is a unique organization that focuses on public education to
-      individuals and families dealing with many types of illnesses, diseases
-      and disabilities as well as education on other needs of elders. Elder Help
-      will provide assistance and advocacy for elders living in residential care
-      communities who have financial and medical shortfalls and other needs
-      unmet by governmental programs.
+      individuals and families dealing with many types of illnesses, diseases,
+      and disabilities as well as education on other needs of elders.
+    </p>
+    <p className="who-we-are-container__vision-content">
+      Elder Help will provide assistance and advocacy for elders living in
+      residential care communities. Many of these facilities have financial and
+      medical shortfalls and other needs unmet by governmental programs.
+    </p>
+    <h3 className="who-we-are-container__partnerships-title">Partnerships</h3>
+    <p className="who-we-are-container__partnerships-content">
+      Collaboration is at the core of everything we do. We know that no single
+      organization can solve the complex set of issues contributing to local and
+      national issues.
+    </p>
+    <Hyperlink
+      href="/take-action"
+      title="Get Involved"
+      className="who-we-are-container__cta"
+    />
+    <figure className="who-we-are-container__chamber-figure">
+      <Image className="who-we-are-container__figure-image" {...chamberImage} />
+      <figcaption className="who-we-are-container__figure-caption">
+        Pictured with Chamber President, Holly Staples (Center), are President,
+        Dana Rabon (Right) and Vice President, Audrey Clark (Left)
+      </figcaption>
+    </figure>
+    <p className="who-we-are-container__partnerships-content">
+      We champion partnerships that bring together expertise, ideas, tools and
+      resources in support of the shared goal of creating a better tomorrow.
     </p>
   </div>
 );
