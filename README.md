f26d5ee3d84b708d91f4b43218c145d41ddeafc0
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-03-16 22:10:03 -0400

    Updating for some text in what we do

--- README.md
+++ README.md
@@ -19,3 +19,5 @@ Code base for Elder Help NC
 ## Photo Credits
 
 1. Photo by <a href="https://unsplash.com/@shanerounce?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Shane Rounce</a> on <a href="/s/photos/community?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+1. Photo by <a href="https://unsplash.com/@knurpselknie?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Georg Arthur Pflueger</a> on <a href="/collections/10480186/senior-care-canada?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
--- src/containers/what-we-do-container/what-we-do-container.jsx
+++ src/containers/what-we-do-container/what-we-do-container.jsx
@@ -7,7 +7,18 @@ const WhatWeDoContainer = () => (
   <div className="what-we-do-container">
     <h2 className="what-we-do-container__main-title">What We Do</h2>
     <p className="what-we-do-container__main-content">
-      <p>Participated in Adopt-A-Senior program</p>
+      <p>
+        Being a part of a community is not something we take lightly. Our
+        mission allows us to bring together different audiences. This mingling
+        allows for educational opportunites in the needs of the senior community
+        as well as provide much needed services.
+      </p>
+      <p>
+        Participated in the Adopt A Senior program which allows us to bring a
+        personal touch to Seniors by directly providing them with some outside
+        contact as well as with gifts to let them know they are not forgotten
+        and alone.
+      </p>
       <p>Community dementia awareness seminar</p>
       <p>Donated incontinence supplies to the tornado victims of Bertie</p>
     </p>
