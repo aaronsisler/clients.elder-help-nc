3f2b9ba60b76135d2b2985d0db0068ff80bb1f2d
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-04-13 20:57:41 -0400

    Adding in the take-action and CTA for landing page

--- src/containers/take-action-container/take-action-container.jsx
+++ src/containers/take-action-container/take-action-container.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import ContactAddress from "../../components/contact-address";
 
 import "./take-action-container.scss";
 
 const TakeActionContainer = () => (
-  <div className="take-action-container">TakeActionContainer</div>
+  <div className="take-action-container">
+    <div className="take-action-container__title">How can you help?</div>
+    <div className="take-action-container__content">
+      With the pandemic still affecting many of our opportunities that allow the
+      general public to be involved, we find the best way to support our mission
+      is with a donation. Currently we are only able to accept checks. Thes
+      should be made out to Elder Help.
+    </div>
+    <ContactAddress />
+  </div>
 );
 
 export default TakeActionContainer;
